3a9144c5f232b1875ab7e9faddbb8c553195b6d9
TheValour <hellowroldexe18@gmail.com>
2023-11-27 23:07:48 +0530

    adding front end to CHP

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import pickle
-from flask import Flask, request, app, jsonify, url_for, render_template
+from flask import Flask, request, jsonify, render_template
 
 import numpy as np
 import pandas as pd
 
 app = Flask(__name__)
 
-## Loading the model 
+# Loading the model
 regmodel = pickle.load(open('regModel.pkl', 'rb'))
 scaler = pickle.load(open('scaling.pkl', 'rb'))
 
@@ -16,13 +16,11 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.json['data']
-    newData = np.array(list(data.values())).reshape(1, -1)
-    print(newData)
-    newData  = scaler.transform(newData)
-    output = regmodel.predict(newData)
-    print(output[0])
-    return jsonify(output[0])
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    output = regmodel.predict(final_input)[0]
+    return render_template("home.html", prediction_text="The Price is {}".format(output))
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
